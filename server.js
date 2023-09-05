4a407e895ad7929182851db1bb51abbd243f574d
kkh061101 <kkh061101@naver.com>
2023-09-05 21:09:36 +0900

    get that code and state

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
-const xpress = require('express');
-const app = xpress();
+const express = require('express');
+const app = express();
 const port = 3333;
+const cors = require('cors');
 
-const SpotifyClientId = "267348cebe4641798a27705a51f66395";
-const SpotyfyClientSecret = '267348cebe4641798a27705a51f66395';
-const redirectUrl = 'http://localhost:3003/';
-
-const generateRandomString = (num) => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  let result = '';
-  const charactersLength = characters.length;
-  for (let i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-
-  return result;
-}
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (rq, rs) => {
   rs.send("Backend root");
   console.log(rs.connection.remoteAddress);
 });
 
-app.get('/login', (rq, rs) => {
-  const state = generateRandomString(16);
-  const scope = '';
-  rs.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${SpotifyClientId}&scope=${scope}&redirect_uri=${redirectUrl}&state=${state}`);
-});;
-
 app.post('/getTokens', (rq, rs) => {
-  console.log(rq.body);
+  const { code, state } = rq.body;
+  if (code && state) {
+    rs.sendStatus(200);
+  } else {
+    rs.sendStatus(502);
+  }
 });
 
 app.listen(port, e => {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,27 @@
+"use client"
 import PlaylistAtom from "@/components/playlistAtom";
+import axios from "axios";
+import { useEffect } from "react";
+
+const url = 'http://localhost:3333';
 
 export default function Home() {
+  const queries = new URLSearchParams(location.search);
+  const getTokens = async e => {
+    await axios.post(`${url}/getTokens`, {
+      code: queries.get('code'),
+      state: queries.get('state')
+    }).then(e => {
+      console.log(e);
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+  useEffect(e => {
+    if (queries.size !== 0) {
+      getTokens();
+    }
+  }, [])
   return <>
     <PlaylistAtom artist={'신해철'} title={'그대에게'} playingtime={'0:30'} playtime={'4:30'} />
   </>;
