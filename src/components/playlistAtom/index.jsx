7755fb6b718834ec4159a8af9c805a6b5c104c2c
kkh061101 <kkh061101@naver.com>
2023-09-07 14:33:19 +0900

    playlist 만들기

--- src/components/playlistAtom/index.jsx
+++ src/components/playlistAtom/index.jsx
@@ -1,16 +1,36 @@
 "use client"
 import axios from 'axios';
 import * as S from './style';
+import { useState } from 'react';
 
 const url = 'https://api.spotify.com/v1';
 
-export default function PlaylistAtom({ img, title, artist, id, type, playingtime, playtime, artistId }) {
+export default function PlaylistAtom({ img, title, artist, id, type, playingtime, playtime, artistId, isInPlay }) {
+  const [toggle, setToggle] = useState(isInPlay);
   const getMusicData = async e => {
     await axios.get(`${url}/audio-analysis/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem('access')}` } }).then(e => {
       console.log(e.data);
     }).catch(e => {
       console.log(e);
     })
+  }
+  const listcontrol = e => {
+    if (type === 'track') {
+      let list = [];
+      list = JSON.parse(localStorage.getItem('list'));
+      if (list === null) {
+        list = [];
+      }
+      if (!toggle) {
+        list.push(id);
+        console.log(list);
+      } else if (toggle) {
+        list = list.filter(track => track !== id);
+        console.log(list);
+      }
+      localStorage.setItem('list', JSON.stringify(list));
+      setToggle(a => !a);
+    }
   }
   return <S.PlayAtom>
     <img src={img} alt="alt" onClick={e => {
@@ -27,5 +47,6 @@ export default function PlaylistAtom({ img, title, artist, id, type, playingtime
     </div>
     <div className="playingtime">{playingtime}</div>
     <div className="playtime">{playtime}</div>
+    {type === "track" && <div className='isInPlay' onClick={e => { listcontrol() }}>{toggle ? '-' : '+'}</div>}
   </S.PlayAtom>;
 }
--- src/components/playlistAtom/style.js
+++ src/components/playlistAtom/style.js
@@ -19,6 +19,8 @@ export const PlayAtom = styled.div`
   }
   .title{
     cursor: pointer;
+    max-width: 300px;
+    min-width: 200px;
     &:hover{
       text-decoration-line: underline;
     }
@@ -28,5 +30,19 @@ export const PlayAtom = styled.div`
     &:hover{
       text-decoration-line: underline;
     }
+  }
+  .isInPlay{
+    cursor: pointer;
+    font-size: 20px;
+    width: 30px;
+    height: 30px;
+    border-radius: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover{
+      background-color: lightgray;
+    }
+    transition: ease 0.2s all;
   }
 `
