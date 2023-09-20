b3463169f386b0a02a15b0f8e6c5da7302f45856
kkh061101 <kkh061101@naver.com>
2023-09-20 23:50:51 +0900

    저장

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -5,22 +5,22 @@ import axios from 'axios';
 
 export default function Player() {
   const [audio, setAudio] = useState(new Audio());
-  const [id, setId] = useState();
+  const [id, setId] = useState(localStorage.getItem('now_playing_id'));
   const [playingtime, setPlayingtime] = useState(1000000);
   const [play, setPlay] = useState(false);
   const [currentT, setCurrentT] = useState(0);
   const [durationT, setDurationT] = useState(`${(playingtime - playingtime % 60000) / 60000}:${((playingtime % 60000 - (playingtime % 60000) % 1000) / 1000).toString().padStart(2, '0')}`);
 
-  const getMusicUrl = async e => {
+  const getMusicUrl = async id => {
     await axios.get(`https://api.spotifydown.com/download/${id}`).then(e => {
-      setAudio(`https://cors.spotifydown.com/${e.data.link}`);
+      console.log(e.data)
+      setAudio(new Audio(`https://cors.spotifydown.com/${e.data.link}`));
     }).catch(e => {
       console.log(e);
     })
   }
   useEffect(e => {
-    console.log(localStorage.getItem('now_playing_id'));
-    setId(localStorage.getItem('now_playing_id'));
+    getMusicUrl(id);
   }, []);
   useEffect(e => {
     if (id) {
@@ -28,9 +28,7 @@ export default function Player() {
       setPlay(false);
       setCurrentT(0);
       setDurationT(`${(playingtime - playingtime % 60000) / 60000}:${((playingtime % 60000 - (playingtime % 60000) % 1000) / 1000).toString().padStart(2, '0')}`);
-      const newAudio = new Audio(id);
-      setAudio(newAudio);
-      getMusicUrl();
+      getMusicUrl(id);
     }
   }, [id]);
   useEffect(e => {
@@ -42,6 +40,6 @@ export default function Player() {
     }
   }, [play]);
   return <S.Player>
-    <button onClick={e => setPlay(a => !a)}>button</button>
+    <button onClick={e => setPlay(a => !a)}>{play ? '⏸' : '▶'}</button>
   </S.Player>;
 }
