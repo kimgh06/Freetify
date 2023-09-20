45451c837373ca5a6e13b4e948396e9333cfd972
kkh061101 <kkh061101@naver.com>
2023-09-20 09:38:53 +0900

    seconds => min:sec

--- src/components/playlistAtom/index.jsx
+++ src/components/playlistAtom/index.jsx
@@ -7,12 +7,12 @@ import Link from 'next/link';
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 const url = 'https://api.spotify.com/v1';
 
-export default function PlaylistAtom({ fullAudio, preview, img, title, artist, id, type, playingtime, artistId, isInPlay, album }) {
+export default function PlaylistAtom({ img, title, artist, id, type, playingtime, artistId, isInPlay, album }) {
   const [toggle, setToggle] = useState(isInPlay);
   const [audio, setAudio] = useState(new Audio());
   const [currentT, setCurrentT] = useState(0);
-  const [durationT, setDurationT] = useState(29);
-  const [full, setFull] = useState(preview);
+  const [durationT, setDurationT] = useState(`${(playingtime - playingtime % 60000) / 60000}:${((playingtime % 60000 - (playingtime % 60000) % 1000) / 1000).toString().padStart(2, '0')}`);
+  const [full, setFull] = useState(null);
   const [play, setPlay] = useState(false);
   const getMusicData = async e => {
     // await axios.get(`${backendUrl}/geturl?url=${url}/${type}/${id}&authorization=${`Bearer ${localStorage.getItem('access')}`}`).then(e => {
@@ -32,7 +32,7 @@ export default function PlaylistAtom({ fullAudio, preview, img, title, artist, i
     });
   }
   const getMusicUrl = async e => {
-    await axios.get(e).then(e => {
+    await axios.get(`https://api.spotifydown.com/download/${id}`).then(e => {
       setFull(`https://cors.spotifydown.com/${e.data.link}`);
     }).catch(e => {
       console.log(e);
@@ -56,27 +56,28 @@ export default function PlaylistAtom({ fullAudio, preview, img, title, artist, i
     }
   }
   useEffect(e => {
+    if (id && type === 'track') {
+      getMusicUrl();
+    }
+  }, [id]);
+  useEffect(e => {
     if (full) {
       audio.pause();
       setPlay(false);
       setCurrentT(0);
-      setAudio(new Audio(full));
+      const newAudio = new Audio(full);
+      setAudio(newAudio);
     }
   }, [full]);
-  useEffect(e => {
-    if (fullAudio) {
-      getMusicUrl(fullAudio);
+  audio.addEventListener('timeupdate', e => {
+    const { currentTime, duration } = audio;
+    setCurrentT(`${(currentTime - currentTime % 60) / 60}:${((currentTime % 60 - (currentTime % 60) % 1) / 1).toString().padStart(2, '0')}`);
+    setDurationT(`${(duration - duration % 60) / 60}:${((duration % 60 - (duration % 60) % 1) / 1).toString().padStart(2, '0')}`);
+    if (duration - currentTime === 0) {
+      setPlay(false);
     }
-  }, []);
+  });
   useEffect(e => {
-    audio.addEventListener('timeupdate', e => {
-      const { currentTime, duration } = audio;
-      setCurrentT(Math.floor(currentTime));
-      setDurationT(Math.floor(duration));
-      if (duration - currentTime === 0) {
-        setPlay(false);
-      }
-    });
     audio.volume = 0.5;
     if (play) {
       audio.play();
@@ -93,12 +94,12 @@ export default function PlaylistAtom({ fullAudio, preview, img, title, artist, i
       <Link className="title" href={album ? `/album/${album?.id}` : '#'} >{title}</Link>&nbsp;
       <Link className="artist" href={`/artist/${artistId}`}>{artist}</Link>
     </div>
-    {playingtime && <div className="playingtime">{`${(playingtime - playingtime % 60000) / 60000}:${((playingtime % 60000 - (playingtime % 60000) % 1000) / 1000).toString().padStart(2, '0')}`}</div>}
+    {playingtime && <div className="playingtime">{durationT}</div>}
     {type === "track" && <div className='isInPlay' onClick={listcontrol}>{toggle ? '-' : '+'}</div>}
-    {full && <div className='audio'>
-      <div className='bar' style={{ width: `${currentT / durationT * 12}vw` }} />
+    {full ? <div className='audio'>
+      <div className='bar' style={{ width: `${audio.currentTime / audio.duration * 12}vw` }} />
       <button onClick={e => setPlay(a => !a)}>{play ? '⏸' : '▶'}</button>
       {currentT}/{durationT}
-    </div>}
+    </div> : type === 'track' && "..."}
   </S.PlayAtom>;
 }
