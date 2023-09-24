65597760151ac1cccf9abceb466f3b68e7883eb5
kkh061101 <kkh061101@naver.com>
2023-09-24 18:38:23 +0900

    recoil-persist audio 못 알아 먹음

--- src/app/recoilStates.js
+++ src/app/recoilStates.js
@@ -1,6 +1,4 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
-const { persistAtom } = recoilPersist();
 export const NowPlayingId = atom({
   key: 'NowPlayingId',
   default: ''
