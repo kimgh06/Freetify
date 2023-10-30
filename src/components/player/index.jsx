11faacbf3ce5dcffe57a2dacb535877a9dc45022
kkh061101 <kkh061101@naver.com>
2023-10-30 15:10:17 +0900

    endpoint 변경

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -23,7 +23,7 @@ export default function Player() {
     // const api_key = `AIzaSyDa4uItii79UYuFou4x3w1-gQyJkkvZF6w`;
     const api_key = `AIzaSyDAhA1LZRQhWKFlrpVqZN2Egb8LXJ6pScY`;
     // const Back_url = 'https://port-0-spotify-jvpb2mlo5rz7z8.sel5.cloudtype.app';
-    const Back_url = '/api/get_video'
+    const Back_url = '/api'
     await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${api_key}&q=${title}+${artist}&videoCategory=10&type=video`)
       .then(async e => {
         const urlId = e.data.items[0].id.videoId;
