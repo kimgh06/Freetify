15803a1963abd37ffa7ba27e2a64204581717240
kkh061101 <kkh061101@naver.com>
2023-12-05 17:47:06 +0900

    bug fixing

--- src/app/api/playlist/route.jsx
+++ src/app/api/playlist/route.jsx
@@ -1,35 +1,33 @@
 import { NextResponse } from "next/server";
-import jwt from 'jsonwebtoken'
 import { useQuery } from "@/app/useQuery";
+import jwt from 'jsonwebtoken'
 const secret = process.env.NEXT_PUBLIC_AUTH_JWT_ACCESS_SECRET;
 
-async function handleRequest(req, response, query) {
+export async function POST(req, response) {
   const Auth = jwt.verify(req.headers.get('Authorization'), secret);
   if (new Date().getTime() > Auth['exp']) {
     return NextResponse.json({ msg: "Need to refresh" }, { status: 403 })
   }
   const { song_id } = await req.json();
-
-  const { err, res } = await query(Auth['user_id'], song_id);
-
+  const { err, res } = await useQuery(`insert into playlist values(${Auth['user_id']}, '${song_id}')`)
   if (err !== null) {
     if (err['errno'] === 1062) {
       return NextResponse.json({ msg: 'Already exists' }, { status: 400 })
     }
     return NextResponse.json({ msg: 'serverError' }, { status: 500 })
   }
-
-  return NextResponse.json({ msg: 'Success' });
+  return NextResponse.json({ msg: 'inserted' });
 }
 
-export async function POST(req, response) {
-  return handleRequest(req, response, async (user_id, song_id) => {
-    return await useQuery(`insert into playlist values(${user_id}, '${song_id}')`);
-  });
-}
-
-export async function DELETE(req, response) {
-  return handleRequest(req, response, async (user_id, song_id) => {
-    return await useQuery(`delete from playlist where user_id = ${user_id} and song_id = '${song_id}'`);
-  });
-}
+// export async function DELETE(req, response) {
+//   const Auth = jwt.verify(req.headers.get('Authorization'), secret);
+//   if (new Date().getTime() > Auth['exp']) {
+//     return NextResponse.json({ msg: "Need to refresh" }, { status: 403 })
+//   }
+//   const { song_id } = await req.json();
+//   const { err, res } = await useQuery(`delete from playlist where user_id = ${Auth['user_id']} and song_id = '${song_id}'`)
+//   if (err !== null) {
+//     return NextResponse.json({ msg: 'serverError' }, { status: 500 })
+//   }
+//   return NextResponse.json({ msg: 'deleted' });
+// }
