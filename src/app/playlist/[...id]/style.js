c10120891ac32b956cdece4539e203c79f7bfb67
kkh061101 <kkh061101@naver.com>
2023-12-10 18:36:23 +0900

    playlist style has changed

--- src/app/playlist/[...id]/style.js
+++ src/app/playlist/[...id]/style.js
@@ -2,7 +2,8 @@ import { styled } from "styled-components";
 
 export const Playlist = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   main{
     h1{
       border-bottom: 8px solid lightgray;
