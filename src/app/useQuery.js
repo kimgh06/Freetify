177e47c2129a14f725442e6ca1ef554179201af8
kkh061101 <kkh061101@naver.com>
2023-12-11 15:31:55 +0900

    skip {} at usequery

--- src/app/useQuery.js
+++ src/app/useQuery.js
@@ -1,19 +1,17 @@
 import mysql2 from 'mysql2';
 
-export const useQuery = query => {
-  const Connection = mysql2.createConnection({
-    host: process.env.NEXT_PUBLIC_SQL_HOST,
-    port: '3306',
-    user: process.env.NEXT_PUBLIC_SQL_USR,
-    password: process.env.NEXT_PUBLIC_SQL_PWD,
-    database: process.env.NEXT_PUBLIC_SQL_DATABASE
-  })
+export const useQuery = query =>
+  new Promise((resolve, reject) => {
+    const Connection = mysql2.createConnection({
+      host: process.env.NEXT_PUBLIC_SQL_HOST,
+      port: '3306',
+      user: process.env.NEXT_PUBLIC_SQL_USR,
+      password: process.env.NEXT_PUBLIC_SQL_PWD,
+      database: process.env.NEXT_PUBLIC_SQL_DATABASE
+    })
 
-  const pro = new Promise((resolve, reject) => {
     const result = Connection.query(query, (err, res, fields) => {
       resolve({ err, res, fields })
       Connection.end()
     })
   })
-  return pro;
-}
