6f7c3fd0cb9474774f43494f2859d074db0771ab
kkh061101 <kkh061101@naver.com>
2023-12-12 14:19:29 +0900

    prerendering issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-5N5SR26M" />
+      {/* <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-5N5SR26M" />
       <Script id="google-analytics">
         {`
         window.dataLayer = window.dataLayer || [];
@@ -20,13 +20,13 @@ export default function RootLayout({ children }) {
         gtag('js', new Date());
 
         gtag('config', 'GA_MEASUREMENT_ID');
-      `}</Script>
+      `}</Script> */}
       <body>
         {children}
       </body>
       {/* <!-- Google Tag Manager (noscript) --> */}
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N5SR26M"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N5SR26M"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
       {/* <!-- End Google Tag Manager (noscript) --> */}
     </html>
   )
