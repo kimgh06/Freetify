7537364dd71ebc7d626197c4f986f4479c4f0245
kkh061101 <kkh061101@naver.com>
2023-12-12 14:24:11 +0900

    deleted comment

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,10 +24,6 @@ export default function RootLayout({ children }) {
       <body>
         {children}
       </body>
-      {/* <!-- Google Tag Manager (noscript) --> */}
-      {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N5SR26M"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
-      {/* <!-- End Google Tag Manager (noscript) --> */}
     </html>
   )
 }
