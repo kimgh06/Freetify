5d8a8f1c1a642496d2b5d8f35ce8a31eaadc94ac
kimgh06 <kkh061101@naver.com>
2024-01-07 18:17:24 +0900

    delete useless opengraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,11 +23,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Freetify" />
+        <meta property="title" content="Freetify" />
         <link rel='icon' href='/favicon.ico' />
         <meta property="og:image" content="/opengraph-image.png" />
         {/* <meta property='og:url' content='https://freetify.vercel.app' /> */}
-        <meta property='og:description' content='A free online music player' />
+        <meta property='description' content='A free online music player' />
         <meta property='og:site_name' content='Freetify' />
         <meta name="google-site-verification" content="q9McqJBLT8ZooTZpbEQYsi_Mr13MbveBtl4j77XLtrc" />      </head>
       <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-5N5SR26M" />
