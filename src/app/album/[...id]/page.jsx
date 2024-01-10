6427988201c7d0496ef8294b9dcdaa391e33ee9c
kimgh06 <kkh061101@naver.com>
2024-01-10 21:08:32 +0900

    album generate metadata

--- src/app/album/[...id]/page.jsx
+++ src/app/album/[...id]/page.jsx
@@ -1,77 +1,63 @@
-"use client"
-import Navi from '@/components/nav';
-import * as S from './style';
-import axios from "axios";
-import { useEffect, useState } from "react"
-import Link from 'next/link';
-import PlaylistAtom from '@/components/playlistAtom';
-import { RecoilRoot, useRecoilState } from 'recoil';
-import { AccessToken } from '@/app/recoilStates';
+import axios from 'axios';
+import { Album } from './albumpage';
 
-export default function App({ params }) {
-  const id = params.id[0];
-  return <RecoilRoot>
-    <InnerContent id={id} />
-  </RecoilRoot>;
+
+export async function generateMetadata({ params }) {
+  if (!params['id'][0]) {
+    return
+  }
+  // const info = await getPlaylistAtoms(params['id'][0]);
+  // const description = `playlist, ${info.length} songs`
+  return {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_AUTH_URL),
+    openGraph: {
+      title: `${params['id'][0]}` || 'playlist',
+      // description: description || 'description'
+    }
+  }
 }
 
-function InnerContent({ id }) {
-  const url = 'https://api.spotify.com/v1';
-  const [albumInfo, setAlbumInfo] = useState(null);
-  const [totalDuration, setTotalDuration] = useState('0');
-  const [access, setAccess] = useRecoilState(AccessToken);
-  const [tracks, setTracks] = useState([]);
-  const getAlbumInfos = async e => {
-    await axios.get(`${url}/albums/${id}`, { headers: { Authorization: `Bearer ${access}` } })
-      .then(e => {
-        console.log(e.data);
-        setAlbumInfo(e.data);
-        setTracks(e.data.tracks.items);
-        let TrackList = [];
-        let sum = 0;
-        e.data.tracks.items.forEach(items => {
-          TrackList.push(items.id);
-          sum += items.duration_ms
-        });
-        setTotalDuration(`${Math.floor(sum / 60 / 1000)}m ${((sum % 60000 - (sum % 60000) % 1000) / 1000).toString().padStart(2, '0')}s`)
-        localStorage.setItem('TrackList', `${TrackList}`);
-        document.title = e.data.name;
-      }).catch(e => {
-        console.log(e);
-      });
+const getTrackinfos = async ids => {
+  const access = await refresh_token();
+  if (ids) {
+    return await axios.get(`https://api.spotify.com/v1/tracks?ids=${ids}`, { headers: { Authorization: `Bearer ${access}` } }).then(e => {
+      return e.data.tracks
+    }).catch(e => {
+      console.log(e);
+    });
   }
-  useEffect(e => {
-    if (access) {
-      getAlbumInfos();
-    }
-  }, [access]);
-  return <>
-    <Navi />
-    <S.AlbumInfos>
-      <div className='header'>
-        <img src={albumInfo?.images[1].url} alt='img' />
-        <div className='description'>
-          <h1>{albumInfo?.name}</h1>
-          <div className='information'>
-            {albumInfo?.artists.map((i, n) => <Link key={n} href={`/artist/${i?.id}`}>{i.name}</Link>)}
-            <span>{albumInfo?.tracks.items.length} {albumInfo?.tracks.items.length === 1 ? "track" : "tracks"}</span>&nbsp;&nbsp;
-            <span>{totalDuration}</span>&nbsp;&nbsp;
-            <span>Released on {albumInfo?.release_date.substr(0, 4)}</span>
-          </div>
-        </div>
-      </div>
-      <main className='tracks'>
-        {tracks?.map((i, n) => <PlaylistAtom img={albumInfo?.images[2].url} index={n} preview={i?.preview_url} playingtime={i?.duration_ms} key={n} album={i?.album} type={i?.type}
-          id={i?.id} title={i?.name} artist={i?.artists} artistId={i?.artists[0]?.id} isInPlay={e => {
-            let list = [];
-            list = JSON.parse(localStorage.getItem('list'));
-            if (!list) {
-              list = [];
-            }
-            return list.find(a => a === i?.id)
-          }} />)}
-      </main>
-      <div className='end' />
-    </S.AlbumInfos>
-  </>
+}
+
+const getPlaylistAtoms = async (nickname, playlist) => {
+  return await axios.post(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/playlist`, { nickname: nickname, playlist: playlist })
+    .then(async e => {
+      let ids = [];
+      e.data.res.forEach(e => {
+        ids.push(e.song_id)
+      })
+      ids = ids.join(',');
+      let tr = await getTrackinfos(ids);
+      if (!tr) {
+        return;
+      }
+      return tr
+    }).catch(e => {
+      console.log(e);
+    })
+}
+
+
+const refresh_token = async e => {
+  return await axios.patch(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/refresh_token`,
+    { refreshToken: process.env.NEXT_PUBLIC_REFRESH_TOKEN }).then(e => {
+      const info = e.data;
+      return info.access_token;
+    }).catch(e => {
+      console.log(e);
+    })
+}
+
+
+export default function App(props) {
+  return <Album {...props} />;
 }
