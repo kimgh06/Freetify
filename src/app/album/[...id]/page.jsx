3e536a0ccb43a587f9429ecc522c6bc6bf37b5bd
kimgh06 <kkh061101@naver.com>
2024-01-10 22:02:03 +0900

    album generateMetadata

--- src/app/album/[...id]/page.jsx
+++ src/app/album/[...id]/page.jsx
@@ -6,19 +6,18 @@ export async function generateMetadata({ params }) {
   if (!params['id'][0]) {
     return
   }
-  // const info = await getPlaylistAtoms(params['id'][0]);
-  // const description = `playlist, ${info.length} songs`
+  const { title, tracks, author, duration } = await getAlbumInfos(params['id'][0]);
+  const description = `${title} - ${author}, album, ${tracks.length} songs, ${duration}`
   return {
     metadataBase: new URL(process.env.NEXT_PUBLIC_AUTH_URL),
     openGraph: {
-      title: `${params['id'][0]}` || 'playlist',
-      // description: description || 'description'
+      title: `${title} - ${author}` || 'playlist',
+      description: description || 'description'
     }
   }
 }
 
-const getTrackinfos = async ids => {
-  const access = await refresh_token();
+const getTrackinfos = async (ids, access) => {
   if (ids) {
     return await axios.get(`https://api.spotify.com/v1/tracks?ids=${ids}`, { headers: { Authorization: `Bearer ${access}` } }).then(e => {
       return e.data.tracks
@@ -28,24 +27,46 @@ const getTrackinfos = async ids => {
   }
 }
 
-const getPlaylistAtoms = async (nickname, playlist) => {
-  return await axios.post(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/playlist`, { nickname: nickname, playlist: playlist })
+
+const getAlbumInfos = async id => {
+  const url = 'https://api.spotify.com/v1';
+  const access = await refresh_token();
+  return await axios.get(`${url}/albums/${id}`, { headers: { Authorization: `Bearer ${access}` } })
     .then(async e => {
-      let ids = [];
-      e.data.res.forEach(e => {
-        ids.push(e.song_id)
-      })
-      ids = ids.join(',');
-      let tr = await getTrackinfos(ids);
-      if (!tr) {
-        return;
-      }
-      return tr
+      console.log(e.data);
+      // setAlbumInfo(e.data);
+      // setTracks(e.data.tracks.items);
+      let TrackList = [];
+      let sum = 0;
+      e.data.tracks.items.forEach(items => {
+        TrackList.push(items.id);
+        sum += items.duration_ms
+      });
+      let tr = await getTrackinfos(TrackList, access);
+      sum = `${Math.floor(sum / 60 / 1000)}m ${((sum % 60000 - (sum % 60000) % 1000) / 1000).toString().padStart(2, '0')}s`;
+      return { title: e.data.name, author: e.data.artists.map((i, n) => i.name).join(' & '), tracks: tr, duration: sum };
     }).catch(e => {
       console.log(e);
-    })
+    });
 }
 
+// const getPlaylistAtoms = async (nickname, playlist) => {
+//   return await axios.post(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/playlist`, { nickname: nickname, playlist: playlist })
+//     .then(async e => {
+//       let ids = [];
+//       e.data.res.forEach(e => {
+//         ids.push(e.song_id)
+//       })
+//       ids = ids.join(',');
+//       let tr = await getTrackinfos(ids);
+//       if (!tr) {
+//         return;
+//       }
+//       return tr
+//     }).catch(e => {
+//       console.log(e);
+//     })
+// }
 
 const refresh_token = async e => {
   return await axios.patch(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/refresh_token`,
