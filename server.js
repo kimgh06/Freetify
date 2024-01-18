f3de58b7b9c5394dfa9d49c056c987d485888f6f
kimgh06 <kkh061101@naver.com>
2024-01-19 08:50:19 +0900

    remove unnecessary dependencies

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const app = express();
-const port = 3333;
-const cors = require('cors');
-let ip = require('ip');
-const ytdl = require('ytdl-core');
-require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const port = 3333;
+// const cors = require('cors');
+// let ip = require('ip');
+// const ytdl = require('ytdl-core');
+// require('dotenv').config();
 
-const redirect_url = process.env.NEXT_PUBLIC_BACKEND_REDIRECT_URL;
-const SpotifyClientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENTID;
-const SpotyfyClientSecret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENTSECRET;
+// const redirect_url = process.env.NEXT_PUBLIC_BACKEND_REDIRECT_URL;
+// const SpotifyClientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENTID;
+// const SpotyfyClientSecret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENTSECRET;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (rq, rs) => {
-  rs.send("Backend root");
-  console.log(rs.connection.remoteAddress);
-});
+// app.get('/', (rq, rs) => {
+//   rs.send("Backend root");
+//   console.log(rs.connection.remoteAddress);
+// });
 
-app.get('/get_video', (rq, rs) => {
-  const id = rq.query.id;
-  ytdl(`https://youtube.com/watch?v=${id}`, { filter: 'audioonly', quality: 'highestaudio' })
-    .pipe(rs);
-})
+// app.get('/get_video', (rq, rs) => {
+//   const id = rq.query.id;
+//   ytdl(`https://youtube.com/watch?v=${id}`, { filter: 'audioonly', quality: 'highestaudio' })
+//     .pipe(rs);
+// })
 
 
-app.listen(port, e => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, e => {
+//   console.log(`Server is running on port ${port}`);
+// });
