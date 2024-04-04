3e0d282f72bcba46ade9391a03b3a85ea6b980ed
kimgh06 <kkh061101@naver.com>
2024-04-04 13:37:03 +0900

    asdf

--- src/app/myprofile/page.jsx
+++ src/app/myprofile/page.jsx
@@ -20,11 +20,12 @@ function InnerComponent() {
     window.location.href = '/'
     return;
   }
-  if (!window?.localStorage) {
-    console.log('undefined')
+
+  if (!(typeof window !== 'undefined' && window.localStorage)) {
+    nickname = localStorage.getItem('user_nickname');
+  } else {
     return;
   }
-  nickname = localStorage?.getItem('user_nickname')
 
   const getAllPlaylist = async e => {
     return await axios.get(`/api/playlist`, { headers: { 'Authorization': localStorage.getItem('user_access') } })
