867c245f5aac02f5f249e4c3271bd7f1e232137b
kimgh06 <kkh061101@naver.com>
2024-04-04 12:27:24 +0900

    no db

--- src/app/api/get_video/route.jsx
+++ src/app/api/get_video/route.jsx
@@ -47,76 +47,79 @@ export async function GET(req, response) {
     });
   } catch (e) {
     console.log(e);
-  }
-  const info = await getInfo(songId, access);
-  const playingtime = info?.duration_ms
-  let release = info?.album.release_date?.substr(0, 4);
-  let album = info?.album?.name
-  let artist = info?.artists[0].name;
-  let length = info.album.total_tracks;
-  let title = info?.name;
-  artist = artist.replace(/&/g, 'and').replace(/%20/g, " ").replace(/$/g, 's');
-  artist = decodeURIComponent(artist)
-  console.log(title, artist)
-  //앨범 검색=> 트랙찾기
-  let list = (await youtubesearchapi.GetListByKeyword(`${artist} ${album} album`, true, 20)).items;
-  list = list.filter(item => item.type === "playlist" && item);
-  // console.log(list)
-  let url;
-  if (list.length != 0) {
-    let playlist = await youtubesearchapi.GetPlaylistData(list[0].id, 100);
-    let newTitle = title
-      .replace(/\(/g, "")
-      .replace(/\)/g, "")
-      .replace(/-/g, "")
-      .toLowerCase()
-      .replace(/ /g, "")
-    if (newTitle.indexOf('remastered') !== -1) {
-      newTitle = newTitle.substr(0, newTitle.indexOf('remastered'))
-    }
-    for (let i = 0; i < playlist.items.length; i++) {
-      const item = playlist.items[i];
-      let asdf = item.title
+  } finally {
+    const info = await getInfo(songId, access);
+    const playingtime = info?.duration_ms
+    let release = info?.album.release_date?.substr(0, 4);
+    let album = info?.album?.name
+    let artist = info?.artists[0].name;
+    let length = info.album.total_tracks;
+    let title = info?.name;
+    artist = artist.replace(/&/g, 'and').replace(/%20/g, " ").replace(/$/g, 's');
+    artist = decodeURIComponent(artist)
+    console.log(title, artist)
+    //앨범 검색=> 트랙찾기
+    let list = (await youtubesearchapi.GetListByKeyword(`${artist} ${album} album`, true, 20)).items;
+    list = list.filter(item => item.type === "playlist" && item);
+    // console.log(list)
+    let url;
+    if (list.length != 0) {
+      let playlist = await youtubesearchapi.GetPlaylistData(list[0].id, 100);
+      let newTitle = title
         .replace(/\(/g, "")
         .replace(/\)/g, "")
-        .replace(/ft. /g, "feat. ")
         .replace(/-/g, "")
-        .replace(/ /g, "")
         .toLowerCase()
-      if (asdf.indexOf(newTitle) !== -1 ||
-        newTitle.indexOf(asdf) !== -1 ||
-        asdf.indexOf(newTitle.replace(/8/g, '&')) !== -1) {
-        console.log("found", item.title)
-        url = item.id;
-        break;
+        .replace(/ /g, "")
+      if (newTitle.indexOf('remastered') !== -1) {
+        newTitle = newTitle.substr(0, newTitle.indexOf('remastered'))
+      }
+      for (let i = 0; i < playlist.items.length; i++) {
+        const item = playlist.items[i];
+        let asdf = item.title
+          .replace(/\(/g, "")
+          .replace(/\)/g, "")
+          .replace(/ft. /g, "feat. ")
+          .replace(/-/g, "")
+          .replace(/ /g, "")
+          .toLowerCase()
+        if (asdf.indexOf(newTitle) !== -1 ||
+          newTitle.indexOf(asdf) !== -1 ||
+          asdf.indexOf(newTitle.replace(/8/g, '&')) !== -1) {
+          console.log("found", item.title)
+          url = item.id;
+          break;
+        }
       }
     }
-  }
-  if (!url) {
-    //싱글 앨범일 경우거나 못 찾았거나
-    list = (await youtubesearchapi.GetListByKeyword(`${artist} ${title} lyrics`))
-      .items.filter(e => {
+    if (!url) {
+      //싱글 앨범일 경우거나 못 찾았거나
+      console.log(title, artist)
+      list = (await youtubesearchapi.GetListByKeyword(`${artist} ${title}`, false, 20)).items;
+      // console.log(list)
+      list = list.filter(e => {
         let duration
         if (e?.length?.simpleText) {
           duration = (parseInt(e.length.simpleText.split(':')[0]) * 60000 + parseInt(e.length.simpleText.split(':')[1] * 1000))
         }
         return e.type !== 'channel' && (duration ? duration < playingtime + 1000 : '')
       });
-    console.log("can't found on playlists", list[0].title)
-    url = list[0].id
-  }
-
-  try {
-    if (url) {
-      const stream = ytdl(`https://youtube.com/watch?v=${url}`, { filter: 'audioonly', quality: 'highestaudio', format: 'mp3' })
-      const response = new Response(stream);
-      response.headers.set('content-type', 'audio/mp3')
-      response.headers.set('connection', 'keep-alive');
-      console.log('complete to send datas')
-      return response
+      console.log("can't found on playlists", list[0].title)
+      url = list[0].id
     }
-    throw "no datas"
-  } catch (e) {
-    return NextResponse.json({ err: e }, { status: 500 })
+
+    try {
+      if (url) {
+        const stream = ytdl(`https://youtube.com/watch?v=${url}`, { filter: 'audioonly', quality: 'highestaudio', format: 'mp3' })
+        const response = new Response(stream);
+        response.headers.set('content-type', 'audio/mp3')
+        response.headers.set('connection', 'keep-alive');
+        console.log('complete to send datas')
+        return response
+      }
+      throw "no datas"
+    } catch (e) {
+      return NextResponse.json({ err: e }, { status: 500 })
+    }
   }
 }
--- src/app/api/login/route.jsx
+++ src/app/api/login/route.jsx
@@ -14,34 +14,38 @@ const Connection = mysql2.createPool({
 
 export async function POST(req, response) {
   const { email, pw } = await req.json();
-  let query = `select * from user_info where email = '${email}' and password = '${pw}'`;
-  let res, err = null;
-  await Connection.query(query).then(e => {
-    // console.log(e[0])
-    res = e[0]
-  }).catch(e => {
-    // console.log(e)
-    err = e
-  })
+  try {
+    let query = `select * from user_info where email = '${email}' and password = '${pw}'`;
+    let res, err = null;
+    await Connection.query(query).then(e => {
+      // console.log(e[0])
+      res = e[0]
+    }).catch(e => {
+      // console.log(e)
+      err = e
+    })
 
-  if (err !== null) {
-    return NextResponse.json({ msg: 'serverError' }, { status: 500 });
+    if (err !== null) {
+      return NextResponse.json({ msg: 'serverError' }, { status: 500 });
+    }
+    if (res.length <= 0) {
+      return NextResponse.json({ msg: 'Failed to find your account. Check your information.' }, { status: 404 })
+    }
+    const exp = (Math.floor(Date.now() / 1000) + (60 * 30)) * 1000;
+    const AccessToken = jwt.sign({
+      user_id: res[0]['user_id'],
+      pw: pw,
+      exp: (Math.floor(Date.now() / 1000) + (60 * 30)) * 1000 //30분
+    }, process.env.NEXT_PUBLIC_AUTH_JWT_ACCESS_SECRET)
+    const RefreshToken = jwt.sign({
+      user_id: res[0]['user_id'],
+      pw: pw,
+      exp: (Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 3)) * 1000, //3일
+    }, process.env.NEXT_PUBLIC_AUTH_JWT_ACCESS_SECRET)
+    return NextResponse.json({ AccessToken, RefreshToken, exp, nickname: res[0]['nickname'] })
+  } catch (e) {
+    return NextResponse.json({ err }, { status: 5000 })
   }
-  if (res.length <= 0) {
-    return NextResponse.json({ msg: 'Failed to find your account. Check your information.' }, { status: 404 })
-  }
-  const exp = (Math.floor(Date.now() / 1000) + (60 * 30)) * 1000;
-  const AccessToken = jwt.sign({
-    user_id: res[0]['user_id'],
-    pw: pw,
-    exp: (Math.floor(Date.now() / 1000) + (60 * 30)) * 1000 //30분
-  }, process.env.NEXT_PUBLIC_AUTH_JWT_ACCESS_SECRET)
-  const RefreshToken = jwt.sign({
-    user_id: res[0]['user_id'],
-    pw: pw,
-    exp: (Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 3)) * 1000, //3일
-  }, process.env.NEXT_PUBLIC_AUTH_JWT_ACCESS_SECRET)
-  return NextResponse.json({ AccessToken, RefreshToken, exp, nickname: res[0]['nickname'] })
 }
 
 export async function PATCH(req, res) {
