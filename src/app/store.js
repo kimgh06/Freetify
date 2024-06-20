918163401420c34445149753f2021ad9b937ac4e
kimgh06 <kkh061101@naver.com>
2024-06-20 10:21:28 +0900

    주석

--- src/app/store.js
+++ src/app/store.js
@@ -8,8 +8,31 @@ export const Store = create((set) => ({
   Volume: 0.7,
   AddButtonIndex: false
 }))
+// export const NowPlayingId = atom({
+//   key: 'NowPlayingId',
+//   default: '',
+// });
+// export const AudioSrc = atom({
+//   key: 'AudioSrc',
+//   default: '',
+//   effects_UNSTABLE: [persistAtom]
+// })
 // export const AccessToken = atom({
 //   key: 'RefreshToken',
 //   default: '',
 //   effects_UNSTABLE: [persistAtom]
+// })
+// export const PlayingCurrentTime = atom({
+//   key: 'PlayingCurrentTime',
+//   default: 0,
+//   effects_UNSTABLE: [persistAtom]
+// })
+// export const Volume = atom({
+//   key: 'Volume',
+//   default: 0.7,
+//   effects_UNSTABLE: [persistAtom]
+// })
+// export const AddbuttonIndex = atom({
+//   key: 'AddButtonIndex',
+//   default: false
 // })
